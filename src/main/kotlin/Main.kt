9b58f9b9f8747e5562ba8e21c4fc242757ad6e79
Gleb BIller <sweetzr16@gmail.com>
2024-03-25 22:24:14 +0300

    WIP

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,13 +5,9 @@ data class Question<T>(
     val answer: T,
     val difficulty: Difficulty
 )
-
-
-
 enum class Difficulty {
     MEDIUM, HARD, EASY
 }
-
 class Quiz {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
@@ -22,8 +18,15 @@ class Quiz {
         var answered: Int = 3
     }
 }
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
 
-
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
 fun main() {
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+    Quiz.printProgressBar()
 }
