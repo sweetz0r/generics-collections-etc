2d49a83d7854eeb13d1d764c94f7d12340934ad1
Gleb BIller <sweetzr16@gmail.com>
2024-03-25 20:19:27 +0300

    WIP

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,29 @@
 package org.example
 
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
-) {
+    val difficulty: Difficulty
+)
 
+
+
+enum class Difficulty {
+    MEDIUM, HARD, EASY
+}
+
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
 }
 
 
-
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
